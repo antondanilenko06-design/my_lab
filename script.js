6a304576b344b8b8fe7b813da7c4fb5ed9e3f24a
TonyMontano <anton.danilenko.06@mail.ru>
2026-02-27 15:40:58 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-/*let cat1 = prompt("Введите первый катет", " ");
+let cat1 = prompt("Введите первый катет", " ");
 let cat2 = prompt("Введите второй катет", " ");
 let num_cat1 = parseFloat(cat1);
 let num_cat2 = parseFloat(cat2);
@@ -6,4 +6,5 @@ let result =Math.sqrt( num_cat1*num_cat1+num_cat2*num_cat2);
 
 alert("Гипотенуза " + result);
 
-*/
+
+
